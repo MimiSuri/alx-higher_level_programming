c0f42a66e50578761d571332acb8a5971bc80ae7
MimiSuri <miimiisurii@yahoo.com>
2023-03-07 04:42:13 -0800

    update

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# 10-add.py
+# Aminu Umar Hassan <miimiisurii@yahoo.com>
+
+def add(a, b):
+    """Return the addition of a and b."""
+    return (a + b)
---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# 11-pow.py
+# Aminu Umar Hassan <miimiisurii@yahoo.com>
+
+def pow(a, b):
+    """Return a to the power of b."""
+    return (a ** b)
