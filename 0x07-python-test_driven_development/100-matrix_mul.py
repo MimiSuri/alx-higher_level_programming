0de3a1bad35ca8b35bbc7cc4604767e50c129bdd
MimiSuri <miimiisurii@yahoo.com>
2023-04-05 10:24:27 -0700

    updated

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -1,50 +1,46 @@
 #!/usr/bin/python3
-"""Module that contains a function to divide a matrix by a scalar"""
+""" Module mul matrix
+"""
 
 
 def matrix_mul(m_a, m_b):
-    """Multiplies two matrices"""
+    """t multiplies 2 matrices
+    """
     if type(m_a) is not list:
-        raise TypeError("m_a must be a list")
+        raise TypeError('m_a must be a list')
     if type(m_b) is not list:
-        raise TypeError("m_b must be a list")
-    for x in m_a:
-        if type(x) is not list:
-            raise TypeError("m_a must be a list of lists")
-    for x in m_b:
-        if type(x) is not list:
-            raise TypeError("m_b must be a list of lists")
-    if len(m_a) < 1:
+        raise TypeError('m_b must be a list')
+    if all(list(map(lambda x: x == list, list(map(type, m_a))))) is False:
+        raise TypeError('m_a must be a list of lists')
+    if all(list(map(lambda x: x == list, list(map(type, m_b))))) is False:
+        raise TypeError('m_b must be a list of lists')
+    if len(m_a) == 0:
         raise ValueError("m_a can't be empty")
-    if len(m_b) < 1:
-        raise ValueError("m_b can't be empty")
-    arowlen = len(m_a[0])
-    if arowlen < 1:
+    if all(list(map(lambda x: x != 0, list(map(len, m_a))))) is False:
         raise ValueError("m_a can't be empty")
-    browlen = len(m_b[0])
-    if browlen < 1:
+    if len(m_b) == 0:
         raise ValueError("m_b can't be empty")
-    for row in m_a:
-        if len(row) != arowlen:
-            raise TypeError("each row of m_a must should be of the same size")
-        for col in row:
-            if type(col) is not float and type(col) is not int:
-                raise TypeError("m_a should contain only integers or floats")
-    for row in m_b:
-        if len(row) != browlen:
-            raise TypeError("each row of m_b must should be of the same size")
-        for col in row:
-            if type(col) is not float and type(col) is not int:
-                raise TypeError("m_b should contain only integers or floats")
-    if arowlen != len(m_b):
+    if all(list(map(lambda x: x != 0, list(map(len, m_b))))) is False:
+        raise ValueError("m_b can't be empty")
+    if all(list(map(lambda x: all(list(map(lambda x:
+       type(x) == int or type(x) == float, x))), m_a))) is False:
+        raise TypeError('m_a should contain only integers or floats')
+    if all(list(map(lambda x: all(list(map(lambda x:
+       type(x) == int or type(x) == float, x))), m_b))) is False:
+        raise TypeError('m_b should contain only integers or floats')
+    if all(list(map(lambda x: len(x) == len(m_a[0]), m_a))) is False:
+        raise TypeError("each row of m_a must should be of the same size")
+    if all(list(map(lambda x: len(x) == len(m_b[0]), m_b))) is False:
+        raise TypeError("each row of m_b must should be of the same size")
+    if len(m_a[0]) != len(m_b):
         raise ValueError("m_a and m_b can't be multiplied")
-    newmatrix = []
-    for x in range(len(m_a)):
-        newrow = []
-        for y in range(browlen):
-            sum = 0
-            for z in range(arowlen):
-                sum += m_a[x][z] * m_b[z][y]
-            newrow.append(sum)
-        newmatrix.append(newrow)
-    return newmatrix
+    mul = []
+    for i in range(len(m_a)):
+        mul_row = []
+        for j in range(len(m_b[0])):
+            item = 0
+            for k in range(len(m_b)):
+                item += m_a[i][k] * m_b[k][j]
+            mul_row.append(item)
+        mul.append(mul_row)
+    return mul
